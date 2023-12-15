ab19ed6cba921afc8598f336690cba9de285ba67
Marina-LA <marinalopezalet@gmail.com>
2023-12-15 11:40:26 +0100

    Added some new DB queries, and some new Plots.

--- database/db_queries.py
+++ database/db_queries.py
@@ -9,13 +9,11 @@ def count_members_with_papers(conference_name, first_year, last_year):
     for year in range(first_year, last_year+1):
         cursor.execute(f"SELECT COUNT(*) FROM Authors_{year} WHERE member = 1")
         count_all_members = cursor.fetchone()[0]
-
         cursor.execute(f'''SELECT COUNT(DISTINCT A.id_author)
                         FROM Authors_{year} A
                         INNER JOIN "Authors_Papers_{year}" AP ON A.id_author = AP.id_author
                         WHERE A.member = 1''')
         count_members_in_papers = cursor.fetchone()[0]
-
         result[year] = {"Members with papers": count_members_in_papers, "All members": count_all_members}
 
     return result
@@ -63,9 +61,89 @@ def papers_by_countries(conference_name, first_year, last_year):
                         GROUP BY Papers_{year}.id_paper;
                         ''')
 
+        results = cursor.fetchall()
+        result[year] = results
+
+    conn.close()
+    return result
+
+def obtain_institutions_conference(conference_name, first_year, last_year):
+    conn = sqlite3.connect(f'../database/{conference_name}.db')
+    cursor = conn.cursor()
+
+    result = {}
+    for year in range(first_year, last_year + 1):
+        cursor.execute(f'SELECT Name, Place FROM Institutions_{year};')
+        results = cursor.fetchall()
+        result[year] = results
+
+    conn.close()
+    return result
+
+
+def obtain_num_papers_only_us(conference_name, first_year, last_year):
+    conn = sqlite3.connect(f'../database/{conference_name}.db')
+    cursor = conn.cursor()
+
+    result = {}
+    for year in range(first_year, last_year + 1):
+        cursor.execute(f'''SELECT COUNT(DISTINCT p.id_paper) AS num_papers_us
+                        FROM Papers_{year} p
+                        JOIN Authors_Papers_{year} ap ON p.id_paper = ap.id_paper
+                        WHERE NOT EXISTS (
+                            SELECT 1
+                            FROM Authors_Papers_{year} ap_other
+                            JOIN Authors_Institutions_{year} ai_other ON ap_other.id_author = ai_other.id_author
+                            JOIN Institutions_{year} i_other ON ai_other.id_institution = i_other.id_institution
+                            WHERE ap_other.id_paper = p.id_paper
+                              AND i_other.Place <> 'US');
+                        ''')
+        results = cursor.fetchall()[0]
+        result[year] = results[0]
+
+    conn.close()
+    return result
+
+
+def obtain_num_papers(conference_name, first_year, last_year):
+    conn = sqlite3.connect(f'../database/{conference_name}.db')
+    cursor = conn.cursor()
+
+    result = {}
+    for year in range(first_year, last_year + 1):
+        cursor.execute(f'''SELECT COUNT(*) AS Num_Papers
+                        FROM Papers_{year};
+                        ''')
+
+        results = cursor.fetchall()[0]
+        result[year] = results[0]
+
+    conn.close()
+    return result
+
+
+def top_institutions(conference_name, first_year, last_year):
+    conn = sqlite3.connect(f'../database/{conference_name}.db')
+    cursor = conn.cursor()
+
+    result = {}
+    for year in range(first_year, last_year + 1):
+        cursor.execute(f'''SELECT Institutions_{year}.Name AS InstitutionName, Institutions_{year}.Place AS Country, COUNT(DISTINCT Papers_{year}.id_paper) AS PaperCount
+                        FROM Institutions_{year}
+                        JOIN Authors_Institutions_{year} ON Institutions_{year}.id_institution = Authors_Institutions_{year}.id_institution
+                        JOIN Authors_Papers_{year} ON Authors_Institutions_{year}.id_author = Authors_Papers_{year}.id_author
+                        JOIN Papers_{year} ON Authors_Papers_{year}.id_paper = Papers_{year}.id_paper
+                        GROUP BY Institutions_{year}.id_institution
+                        ORDER BY PaperCount DESC
+                        LIMIT 10;
+                        ''')
 
         results = cursor.fetchall()
         result[year] = results
+        """
+        for res in results:
+            print(f'Year: {year} -> Institution: {res[0]}, {res[1]} // Total Counts: {res[2]}')
+        """
 
     conn.close()
     return result
--- main_apps/plots.py
+++ main_apps/plots.py
@@ -4,6 +4,7 @@ import global_data
 import plotly.express as px
 import database.db_queries as db
 import pycountry
+import plotly.graph_objects as go
 
 
 def create_piechart_conf_countries(conference):
@@ -15,10 +16,9 @@ def create_piechart_conf_countries(conference):
         fig = px.pie(
             names=df.index,
             values=df[year],
-            title=f'{conference} {year}',
             template="plotly",
         )
-        fig.update_traces(textinfo="label", textposition="inside")
+        fig.update_traces(textinfo='percent+label', textposition="inside")
         figs.append(fig)
 
     subplot = make_subplots(rows=2, cols=6, specs=[[{'type': 'pie'}] * 6] * 2, subplot_titles=list(df.columns))
@@ -29,57 +29,103 @@ def create_piechart_conf_countries(conference):
 
     subplot.update_layout(
         showlegend=True,
-        title_text=f"<b style='font-size: 24px;'>11 years of {conference}</b>",
+        title_text=f"<b style='font-size: 24px;'>Countries of {conference}</b>",
     )
     subplot.show()
+    #subplot.write_html(f'../html/piechart_{conference}_countries.html')
 
 
-def create_heat_map_countries(conference):
+def scatter_geo_countries(conference):
     data = db.obtain_countries_from_conference(conference, global_data.FIRST_YEAR, global_data.LAST_YEAR)
-    df = pd.DataFrame(data).T.reset_index()
-    df = pd.melt(df, id_vars=['index'], var_name='Country', value_name='Frequency')
-    df = df.rename(columns={'index': 'Year'})
+    df = pd.DataFrame(data)
 
-    # Filter None values
-    df = df[df['Country'] != 'None']
+    new_df = df.reset_index().melt(id_vars='index', var_name='Year', value_name='Repetitions')
+    new_df.columns = ['Country', 'Year', 'Repetitions']
 
-    df['Country_ISO3'] = df['Country'].apply(lambda x: pycountry.countries.get(alpha_2=x).alpha_3 if pd.notnull(x) else None)
+    new_df = new_df[new_df['Country'] != 'None']
+    new_df['Country_ISO3'] = new_df['Country'].apply(lambda x: pycountry.countries.get(alpha_2=x).alpha_3 if pd.notnull(x) else None)
+    new_df['Country Name'] = new_df['Country_ISO3'].apply(lambda x: pycountry.countries.get(alpha_3=x) if pd.notnull(x) else None)
+    new_df.dropna(subset=['Repetitions'], inplace=True)
 
-    #df['Frequency'].fillna(0, inplace=True)
-    df.dropna(subset=['Frequency'], inplace=True)
-
-    fig = px.choropleth(
-        df,
-        locations='Country_ISO3',
-        color='Frequency',
-        color_continuous_scale=px.colors.sequential.Plasma,
-        labels={'Frequency': 'Frequency'},
-        animation_frame='Year',
-        range_color=[1, df['Frequency'].max()]
+    fig = px.scatter_geo(
+        new_df,
+        locations="Country_ISO3",
+        locationmode="ISO-3",
+        hover_name="Country_ISO3",
+        size="Repetitions",
+        color="Repetitions",
+        projection="natural earth",
+        size_max=40,
+        animation_frame="Year",
+        animation_group="Country_ISO3",
+        color_continuous_scale='portland',
+        labels = {'Repetitions': 'Institutions from this country', 'Country_ISO3':'Country Code'}
     )
     fig.update_geos(showland=True, landcolor="lightgray")
+    fig.update_layout(
+        coloraxis=dict(
+            cmin=new_df['Repetitions'].min(),
+            cmax=new_df['Repetitions'].max(),
+            colorbar=dict(
+                title='Number of institutions',
+                title_font = dict(size=18, family="Arial, sans-serif", color="black")
+            )
+        ),
+        showlegend=True,
+        title_text=f"<b style='font-size: 24px;'>Number of institutions from each country in {conference}</b>"
+    )
+    fig.show()
+    #fig.write_html(f'../html/scatter_geo_countries_{conference}.html')
 
+
+
+def linechart_countries_conference():
+    conf_res = {}
+    for conf in global_data.conferences:
+        data = db.obtain_countries_from_conference(conf, global_data.FIRST_YEAR, global_data.LAST_YEAR)
+        conf_res[conf] = data
+
+    df = pd.DataFrame(conf_res)
+    df_melted = pd.melt(df.reset_index(), id_vars=['index'], var_name='Conference', value_name='Repetitions')
+    df_melted['No Data'] =  df_melted['Repetitions'].apply(lambda rep: rep.get('None', 0))
+    df_melted['Num Countries'] = df_melted['Repetitions'].apply(lambda rep: sum(1 for key in rep.keys() if key != 'None'))
+    df_melted = df_melted.rename(columns={'index': 'Year'})
+
+    fig = px.line(df_melted, x='Year', y='Num Countries', color='Conference')
+    line_dash_dict = {'middleware': 'dash', 'cloud': 'dash', 'nsdi': 'dash'}
+    for conference, line_dash in line_dash_dict.items():
+        conference_data = df_melted[df_melted['Conference'] == conference]
+        fig.add_trace(go.Scatter(
+            x=conference_data['Year'],
+            y=conference_data['Num Countries'] + conference_data['No Data'],
+            mode='lines',
+            name=f'{conference} with None Countries',
+            line=dict(dash=line_dash)
+        ))
     fig.update_layout(
         showlegend=True,
-        title_text=f"<b style='font-size: 24px;'>Number of times that a country appeared on {conference}</b>",
+        title_text=f"<b style='font-size: 24px;'>Number of countries per conference</b>",
     )
-    # show plot
     fig.show()
+    #fig.write_html(f'../html/NEW_linechart_num_countries.html')
 
 
 def create_piechart_members_with_papers(conference):
     data = db.count_members_with_papers(conference, global_data.FIRST_YEAR, global_data.LAST_YEAR)
     df = pd.DataFrame(data)
+    df.loc['Members without papers'] = df.loc['All members'] - df.loc['Members with papers']
+    df = df.drop('All members')
 
     figs = []
     for year in df.columns:
         fig = px.pie(
-            names=list(data[year].keys()),
+            names=df.index,
             values=list(data[year].values()),
             title=f'Committee Members with papers in {year}',
-            color_discrete_sequence=px.colors.sequential.Viridis
+            template = 'plotly',
+            hole = 0.3
         )
-        fig.update_traces(textinfo="value", textposition="inside")
+        fig.update_traces(textinfo="percent", textposition="inside")
         figs.append(fig)
 
     subplot = make_subplots(rows=2, cols=6, specs=[[{'type': 'pie'}] * 6] * 2, subplot_titles=list(df.columns))
@@ -90,9 +136,10 @@ def create_piechart_members_with_papers(conference):
 
     subplot.update_layout(
         showlegend=True,
-        title_text=f"<b style='font-size: 24px;'>Committee members that have published a paper in {conference}</b>",
+        title_text=f"<b style='font-size: 24px;'>Members that published a paper in {conference}</b>",
     )
     subplot.show()
+    #subplot.write_html(f'../html/piechart_members_{conference}_with_papers.html')
 
 
 
@@ -113,8 +160,10 @@ def create_barchart_conf_countries():
     values = list(res.values())
 
     fig = px.bar(x=conferences, y=values, labels={'x': 'Conference', 'y': 'Number of Countries'}, color=conferences, color_continuous_scale='Viridis')
-    fig.update_layout(title=f'Number of Countries on each conference', width=800, height=800)
+    fig.update_layout(width=500, height=500, title_text=f"<b style='font-size: 24px;'>Number of Countries on each conference</b>")
     fig.show()
+    #fig.write_html('../html/barchart_countries_conf.html')
+
 
 
 def create_how_many_papers_have_a_country(country):
@@ -146,22 +195,65 @@ def create_how_many_papers_have_a_country(country):
 
     for conf in global_data.conferences:
         df = pd.DataFrame(results_for_conference[conf])
-        fig = px.bar(df, x='Year', y='Value', color='Type',
-                     title=f"<b style='font-size: 24px;'>Number of papers in {conf} that some institution is from {country}</b>",
-                     labels={'Value': 'Number of Papers Published', 'Year': 'Year', 'Type': 'Type of Paper'},
-                     text_auto=True)
+        fig = px.line(df, x='Year', y='Value', color='Type',
+                labels={'Value': 'Number of Papers Published', 'Year': 'Year', 'Type': 'Type of Paper'})
 
         fig.update_traces(name=f'With {country}', selector=dict(name='With Country'))
         fig.update_traces(name=f'Without {country}', selector=dict(name='Without Country'))
-
-        fig.update_xaxes(tickmode='array', tickvals=df['Year'], ticktext=df['Year'])
+        fig.update_layout(
+            showlegend=True,
+            title_text=f"<b style='font-size: 24px;'>{country} institutions on {conf} papers</b>",
+        )
         fig.show()
+        #fig.write_html(f'../html/{conf}_how_many_papers_have_us.html')
+
+
+def linechart_only_us_papers():
+    conf_res = {}
+    for conf in global_data.conferences:
+        data = db.obtain_num_papers_only_us(conf, global_data.FIRST_YEAR, global_data.LAST_YEAR)
+        conf_res[conf] = data
+
+    df = pd.DataFrame(conf_res)
+    df_melted = pd.melt(df.reset_index(), id_vars=['index'], var_name='Conference', value_name='Num Papers')
+    df_melted.columns = ['Year', 'Conference', 'Num Papers']
+
+    fig = px.line(df_melted, x='Year', y='Num Papers', color='Conference')
+    fig.update_layout(
+        showlegend=True,
+        title_text=f"<b style='font-size: 24px;'>Number of papers with only USA institutions</b>",
+    )
+    fig.show()
+    #fig.write_html('../html/linechart_only_us.html')
+
+
+def linechart_num_papers():
+    conf_res = {}
+    for conf in global_data.conferences:
+        data = db.obtain_num_papers(conf, global_data.FIRST_YEAR, global_data.LAST_YEAR)
+        conf_res[conf] = data
+
+    df = pd.DataFrame(conf_res)
+    df_melted = pd.melt(df.reset_index(), id_vars=['index'], var_name='Conference', value_name='Num_Papers')
+    df_melted.columns = ['Year', 'Conference', 'Num Papers']
+
+    fig = px.line(df_melted, x='Year', y='Num Papers', color='Conference')
+    fig.update_layout(
+        showlegend=True,
+        title_text=f"<b style='font-size: 24px;'>Total number of papers</b>",
+    )
+    fig.show()
+    #fig.write_html('../html/linechart_num_papers.html')
+
 
 
 if __name__ == '__main__':
     create_how_many_papers_have_a_country("US")
     create_barchart_conf_countries()
+    linechart_only_us_papers()
+    linechart_num_papers()
+    linechart_countries_conference()
     for conf in global_data.conferences:
+        scatter_geo_countries(conf)
         create_piechart_members_with_papers(conf)
         create_piechart_conf_countries(conf)
-        create_heat_map_countries(conf)
