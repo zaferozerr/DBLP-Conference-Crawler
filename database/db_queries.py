da1586130b17d3dab94104c5bf826a1289eed134
Marina-LA <marinalopezalet@gmail.com>
2023-12-19 18:52:47 +0100

    Changed some of the data from the DB. Changed and added new DB queries. Changed, added and deleted some plots.

--- database/db_queries.py
+++ database/db_queries.py
@@ -24,7 +24,6 @@ def obtain_countries_from_conference(conference_name, first_year, last_year):
     cursor = conn.cursor()
 
     result = {}
-
     for year in range(first_year, last_year + 1):
         cursor.execute(f'''
             SELECT Place, COUNT(*) AS Repetitions
@@ -45,27 +44,34 @@ def obtain_countries_from_conference(conference_name, first_year, last_year):
     return result
 
 
-def papers_by_countries(conference_name, first_year, last_year):
+def papers_by_countries(conference_name, first_year, last_year, country):
     conn = sqlite3.connect(f'../database/{conference_name}.db')
     cursor = conn.cursor()
 
-    result = {}
+    res = {}
     for year in range(first_year, last_year + 1):
-        cursor.execute(f'''
-                        SELECT Papers_{year}.Name AS Paper_Name, GROUP_CONCAT(Institutions_{year}.Place) AS Institution_Places
+        cursor.execute(f'''SELECT 
+                            COUNT(DISTINCT Papers_{year}.id_paper) AS TotalPapers,
+                            COUNT(DISTINCT PapersWithUSAuthors.id_paper) AS PapersWithUSAuthors
                         FROM Papers_{year}
-                        JOIN Authors_Papers_{year} ON Papers_{year}.id_paper = Authors_Papers_{year}.id_paper
-                        JOIN Authors_{year} ON Authors_Papers_{year}.id_author = Authors_{year}.id_author
-                        LEFT JOIN Authors_Institutions_{year} ON Authors_{year}.id_author = Authors_Institutions_{year}.id_author
+                        LEFT JOIN Authors_Papers_{year} ON Papers_{year}.id_paper = Authors_Papers_{year}.id_paper
+                        LEFT JOIN Authors_Institutions_{year} ON Authors_Papers_{year}.id_author = Authors_Institutions_{year}.id_author
                         LEFT JOIN Institutions_{year} ON Authors_Institutions_{year}.id_institution = Institutions_{year}.id_institution
-                        GROUP BY Papers_{year}.id_paper;
+                        LEFT JOIN (
+                            SELECT DISTINCT Papers_{year}.id_paper
+                            FROM Papers_{year}
+                            JOIN Authors_Papers_{year} ON Papers_{year}.id_paper = Authors_Papers_{year}.id_paper
+                            JOIN Authors_Institutions_{year} ON Authors_Papers_{year}.id_author = Authors_Institutions_{year}.id_author
+                            JOIN Institutions_{year} ON Authors_Institutions_{year}.id_institution = Institutions_{year}.id_institution
+                            WHERE Institutions_{year}.Place = '{country}'
+                        ) AS PapersWithUSAuthors ON Papers_{year}.id_paper = PapersWithUSAuthors.id_paper
                         ''')
-
-        results = cursor.fetchall()
-        result[year] = results
+        result = cursor.fetchone()
+        res[year] = {'Total':result[0], 'Only Country':result[1]}
 
     conn.close()
-    return result
+    return res
+
 
 def obtain_institutions_conference(conference_name, first_year, last_year):
     conn = sqlite3.connect(f'../database/{conference_name}.db')
@@ -105,28 +111,12 @@ def obtain_num_papers_only_us(conference_name, first_year, last_year):
     return result
 
 
-def obtain_num_papers(conference_name, first_year, last_year):
-    conn = sqlite3.connect(f'../database/{conference_name}.db')
-    cursor = conn.cursor()
-
-    result = {}
-    for year in range(first_year, last_year + 1):
-        cursor.execute(f'''SELECT COUNT(*) AS Num_Papers
-                        FROM Papers_{year};
-                        ''')
-
-        results = cursor.fetchall()[0]
-        result[year] = results[0]
-
-    conn.close()
-    return result
-
-
 def top_institutions(conference_name, first_year, last_year):
     conn = sqlite3.connect(f'../database/{conference_name}.db')
     cursor = conn.cursor()
 
     result = {}
+    aux = {}
     for year in range(first_year, last_year + 1):
         cursor.execute(f'''SELECT Institutions_{year}.Name AS InstitutionName, Institutions_{year}.Place AS Country, COUNT(DISTINCT Papers_{year}.id_paper) AS PaperCount
                         FROM Institutions_{year}
@@ -140,6 +130,13 @@ def top_institutions(conference_name, first_year, last_year):
 
         results = cursor.fetchall()
         result[year] = results
+        for res in results:
+            aux[res[0]]= aux.get(res[0], 0) + res[2]
+
+    ordered_dict = dict(sorted(aux.items(), key=lambda item: item[1], reverse=True))
+    print(ordered_dict)
+    for r in ordered_dict:
+        print(r)
         """
         for res in results:
             print(f'Year: {year} -> Institution: {res[0]}, {res[1]} // Total Counts: {res[2]}')
@@ -147,3 +144,41 @@ def top_institutions(conference_name, first_year, last_year):
 
     conn.close()
     return result
+
+def obtain_num_papers(conference_name, first_year, last_year):
+    conn = sqlite3.connect(f'../database/{conference_name}.db')
+    cursor = conn.cursor()
+
+    res = {}
+    for year in range(first_year, last_year + 1):
+        cursor.execute(f'''SELECT COUNT(*) FROM Papers_{year}''')
+        result = cursor.fetchone()
+        num_papers = result[0]
+        res[year] = num_papers
+
+    return res
+
+
+
+def obtain_institutions_from_country(conference_name, first_year, last_year, country):
+    conn = sqlite3.connect(f'../database/{conference_name}.db')
+    cursor = conn.cursor()
+
+    res = {}
+    for year in range(first_year, last_year + 1):
+        cursor.execute(f'''SELECT 
+                (SELECT COUNT(*) FROM Institutions_{year}) AS TotalInstitutions,
+                (SELECT COUNT(DISTINCT id_institution) FROM Authors_Institutions_{year} 
+                WHERE id_institution IS NOT NULL AND id_institution IN (SELECT id_institution FROM Institutions_{year} WHERE Place = '{country}')) AS USInstitutions
+                ''')
+
+        result = cursor.fetchone()
+        res[year] = result
+    """
+    suma = 0
+    for elem in res.values():
+        percentage = elem[1]/elem[0]
+        suma = suma + percentage
+    print(suma/len(res.keys())*100)
+    """
+    return res
